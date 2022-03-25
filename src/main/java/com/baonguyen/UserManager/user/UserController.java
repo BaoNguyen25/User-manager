8cd23869e2e95b568bde0deb4f2b6b3fda03e711
BaoNguyen25 <nguyenminhgiabao2501@gmail.com>
2022-03-25 10:09:50 +0700

    Implement add user feature

--- src/main/java/com/baonguyen/UserManager/user/UserController.java
+++ src/main/java/com/baonguyen/UserManager/user/UserController.java
@@ -4,7 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -19,10 +20,27 @@ public class UserController {
     }
 
     @GetMapping("/users")
-    public String showUserList(Model model){
+    public String showUserList(Model model) {
+        // list all users
         List<User> listUsers = (List<User>) userService.listAll();
         model.addAttribute("listUsers", listUsers);
         return "users";
     }
+
+    @GetMapping("/users/new")
+    public String showNewForm(Model model) { // create model to bind data form
+        model.addAttribute("user", new User());
+        return "add_user";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user, RedirectAttributes ra) {
+        //save user to database
+        userService.save(user);
+        ra.addFlashAttribute("message", "The user has been added successfully");
+        return "redirect:/users";
+    }
+
+
 
 }
--- src/main/java/com/baonguyen/UserManager/user/UserService.java
+++ src/main/java/com/baonguyen/UserManager/user/UserService.java
@@ -3,8 +3,6 @@ package com.baonguyen.UserManager.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class UserService {
 
@@ -17,6 +15,10 @@ public class UserService {
 
     public Iterable<User> listAll() {
         return userRepository.findAll();
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
     }
 }
 
