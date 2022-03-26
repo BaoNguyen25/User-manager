380ea41d4471f6b97e3bb6d0a596468118d452b6
BaoNguyen25 <nguyenminhgiabao2501@gmail.com>
2022-03-26 12:32:06 +0700

    Implement update and delete user features

--- src/main/java/com/baonguyen/UserManager/user/UserController.java
+++ src/main/java/com/baonguyen/UserManager/user/UserController.java
@@ -3,8 +3,7 @@ package com.baonguyen.UserManager.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -30,17 +29,48 @@ public class UserController {
     @GetMapping("/users/new")
     public String showNewForm(Model model) { // create model to bind data form
         model.addAttribute("user", new User());
-        return "add_user";
+        model.addAttribute("pageTitle", "Add New User");
+        return "user_form";
     }
 
     @PostMapping("/users/save")
     public String saveUser(User user, RedirectAttributes ra) {
         //save user to database
-        userService.save(user);
-        ra.addFlashAttribute("message", "The user has been added successfully");
+        try {
+            userService.save(user);
+            ra.addFlashAttribute("addMessage", "The user has been saved successfully");
+        }
+        catch(IllegalStateException e) {
+            ra.addFlashAttribute("emailUsed" , e.getMessage());
+        }
         return "redirect:/users";
     }
 
+    @GetMapping("/users/update/{id}")
+    public String showUpdateForm(@PathVariable(name="id") Integer id, Model model, RedirectAttributes ra) throws IllegalStateException {
+        try {
+            //get user from service
+            User user = userService.get(id);
+            model.addAttribute("user", user);
+            model.addAttribute("pageTitle", "Update User (ID: " + id + ")");
+            return "user_form";
+        }
+        catch(IllegalStateException e) {
+            ra.addFlashAttribute("updateMessage", e.getMessage());
+            return "redirect:/users";
+        }
+    }
 
+    @GetMapping("/users/delete/{id}")
+    public String deleteUser(@PathVariable(name="id") Integer id, RedirectAttributes ra) throws IllegalStateException {
+        try {
+            userService.delete(id);
+            ra.addFlashAttribute("deleteMessage", "The user with ID " + id + " has been deleted");
+        }
+        catch(IllegalStateException e) {
+            ra.addFlashAttribute("deleteMessage", e.getMessage());
+        }
+        return "redirect:/users";
+    }
 
 }
--- src/main/java/com/baonguyen/UserManager/user/UserService.java
+++ src/main/java/com/baonguyen/UserManager/user/UserService.java
@@ -3,6 +3,8 @@ package com.baonguyen.UserManager.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
 
@@ -18,7 +20,31 @@ public class UserService {
     }
 
     public void save(User user) {
+        Optional<User> studentOptional = userRepository.findUserByEmail(user.getEmail());
+        if(studentOptional.isPresent()) {
+            throw new IllegalStateException("Email is already used!");
+        }
         userRepository.save(user);
+    }
+
+    public User get(Integer id){
+        Optional<User> userOptional = userRepository.findById(id);
+        if(userOptional.isPresent()) {
+            return userOptional.get();
+        }
+        else {
+            throw new IllegalStateException("User with ID " + id + " does not exists");
+        }
+    }
+
+    public void delete(Integer id) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if(userOptional.isPresent()) {
+            userRepository.deleteById(id);
+        }
+        else {
+            throw new IllegalStateException("User with ID " + id + " does not exists");
+        }
     }
 }
 
